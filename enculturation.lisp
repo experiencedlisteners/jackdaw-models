828cf97914effd0dda06fcf48209512ff0a0b778
Bastiaan van der Weij <bjvanderweij@gmail.com>
2020-11-22 20:13:38 +0100

    Add implementation of the enculturation model

---
+++ enculturation.lisp
@@ -0,0 +1,37 @@
+;;; 
+;;; A jackdaw implementation of the "enculturation model" described in chapter 
+;;; five of Van der Weij (2020) and used in chapter seven of Van der Weij
+;;; (2020).
+;;;
+
+(in-package #:jackdaw)
+
+(defmethod meter-domain ((m enculturation))
+  (loop for m in (meter-parameters m) collect (car (car m))))
+
+(defmodel enculturation ()
+  (training? ioi-domain meter-parameters) 
+  ((M ; meter
+      (^m)
+      (categorical
+       () :parameters (meter-parameters enculturation))
+      (persistent (meter-domain model))
+      :key (lambda (m) (list (getf m 'period) (getf m 'pulses))))
+   (D ; downbeat distance
+      (^d ^p m)
+      (accumulator-model (m)
+			 :training? (training? enculturation))
+      (recursive (loop for ioi in (ioi-domain model)
+		       collect (cons (+ $^p ioi) $^d))
+		 (deterministic '(*)))
+      :formatter (lambda (d) (car d)))
+   (P ; phase
+      (^p m d)
+      (uniform ())
+      (recursive (list (mod (car $d) (car $m)))
+		 (loop for phase below (car $m)
+		       collect phase)))
+   (IOI (d ^p ^ioi)
+	(uniform ())
+	(if (eq (car $d) '*) '(*)
+		(list (- (car $d) $^p))))))
